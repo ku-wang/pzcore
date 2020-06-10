a77019887e575cef5a9aefbde89e8cef41e12386
kwang <kwang@panzura.com>
2020-06-10 17:08:16 +0800

    check ch

--- tmp/check_ch.py
+++ tmp/check_ch.py
@@ -1,4 +1,7 @@
 from paramiko_ssh import ssh_obj
+from log_module import log_m
+
+logger = log_m.logger
 
 master = '10.180.116.11'
 bd_ip = '10.180.116.17'
@@ -9,7 +12,19 @@ ssh_obj = ssh_obj.SSHObj(master)
 
 bd_cmd = "/usr/bin/dplmanager -m aex -a {bd_ip} -p {bd_port} devlist".format(bd_ip=bd_ip, bd_port=bd_port)
 
-cc = ssh_obj.run_cmd(bd_cmd)
-print(cc)
-ips_ids = {}
+cmd1 = bd_cmd + "|grep dpl_ip|awk '{print $2}'"
+cmd2 = bd_cmd + "|grep channel_uuid|awk '{print $2}'"
+
+ips = ssh_obj.run_cmd(cmd1).strip().split('\n')
+ids = ssh_obj.run_cmd(cmd2).strip().split('\n')
+
+for ip in ips:
+    for id in ids:
+        cmd = "/usr/bin/dplmanager -m ch -c {id} -a {ip} -p 10809 stat detail".format(id=id, ip=ip)
+        logger.info(ssh_obj.run_cmd(cmd))
+        del ids[0]
+        break
+
+
+
 
