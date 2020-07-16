a44ac7caa3b8ac5b1952b58b21b436af3f0b0bc7
Chris <kwang@panzura.com>
2020-07-16 16:22:32 +0800

    name main

--- paramiko_ssh/ssh_obj.py
+++ paramiko_ssh/ssh_obj.py
@@ -43,36 +43,36 @@ class SSHObj(object):
         self.close()
         return results
 
+if __name__ == '__main__':
+    # # initial the ssh
+    node1 = "10.180.116.11"
+    # ssh1 = SSHObj(ip='10.199.128.100')
+    ssh2 = SSHObj(node1)
+    #
+    # # create the tmp folder on env
+    folder = "/root/{time}".format(time=time_str)
+    ctf = "mkdir -p {folder}/c-snapshot;mkdir -p {folder}/r-snapshot".format(folder=folder)
+    ssh2.run_cmd(ctf)
 
-# # initial the ssh
-node1 = "10.180.116.11"
-# ssh1 = SSHObj(ip='10.199.128.100')
-ssh2 = SSHObj(node1)
-#
-# # create the tmp folder on env
-folder = "/root/{time}".format(time=time_str)
-ctf = "mkdir -p {folder}/c-snapshot;mkdir -p {folder}/r-snapshot".format(folder=folder)
-ssh2.run_cmd(ctf)
-
-locations = ["app", "snapshot", "restore"]
-
-
-def create_yaml_from_local(resource_location, target_location, ssh_obj):
-    cmds = ''
-    with open(resource_location) as f:
-        for line in f:
-            wr = line.strip('\n')
-            cmd = "echo '{wr}' >> {folder};".format(wr=wr, folder=target_location)
-            cmds = cmds + cmd
-    ssh_obj.run_cmd(cmds)
+    locations = ["app", "snapshot", "restore"]
 
 
-for loc in locations:
-    if loc == "app":
-        target = folder + "/c-snapshot/app.yaml"
-    if loc == "snapshot":
-        target = folder + "/c-snapshot/generate-snapshot-1.yaml"
-    if loc == "restore":
-        target = folder + "/r-snapshot/restore-snapshot-1.yaml"
-    create_yaml_from_local(loc, target, ssh2)
+    def create_yaml_from_local(resource_location, target_location, ssh_obj):
+        cmds = ''
+        with open(resource_location) as f:
+            for line in f:
+                wr = line.strip('\n')
+                cmd = "echo '{wr}' >> {folder};".format(wr=wr, folder=target_location)
+                cmds = cmds + cmd
+        ssh_obj.run_cmd(cmds)
+
+
+    for loc in locations:
+        if loc == "app":
+            target = folder + "/c-snapshot/app.yaml"
+        if loc == "snapshot":
+            target = folder + "/c-snapshot/generate-snapshot-1.yaml"
+        if loc == "restore":
+            target = folder + "/r-snapshot/restore-snapshot-1.yaml"
+        create_yaml_from_local(loc, target, ssh2)
 
