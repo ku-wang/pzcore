6d850afdb1ba20168ff93a07733c17bb822f3d58
Chris <kwang@panzura.com>
2020-09-04 14:30:44 +0800

    some new

--- boto3_module/s3.py
+++ boto3_module/s3.py
@@ -14,13 +14,14 @@ class S3obj(object):
         self.access_key = access_key
         self.secret_key = secret_key
 
+    @property
     def s3_client(self):
         return boto3.client(service_name="s3", endpoint_url=self.endpoint_url, aws_access_key_id=self.access_key,
                             aws_secret_access_key=self.secret_key)
 
     def list_buckets(self):
         buckets = []
-        responce = self.s3_client().list_buckets()
+        responce = self.s3_client.list_buckets()
 
         for bucket in responce['Buckets']:
             buckets.append(bucket['Name'])
@@ -30,21 +31,34 @@ class S3obj(object):
     def list_files_from_bucket(self, bucket):
         files = []
 
-        response = self.s3_client().list_objects_v2(Bucket=bucket)
+        response = self.s3_client.list_objects_v2(Bucket=bucket)
         for file in response['Contents']:
             files.append(file['Key'])
 
         return {bucket: files}
 
     def upload_file(self, source_file_path, bucket_name, upload_as_name):
-        self.s3_client().upload_file(source_file_path, bucket_name, upload_as_name)
+        self.s3_client.upload_file(source_file_path, bucket_name, upload_as_name)
 
     def download_file(self, bucket_name, source_file, download_as_name):
-        self.s3_client().download_file(bucket_name, source_file, download_as_name)
+        self.s3_client.download_file(bucket_name, source_file, download_as_name)
+
+    def delete_file(self, bucket_name, file_path):
+        response = self.s3_client.delete_object(Bucket=bucket_name, Key=file_path)
+
+        if response['ResponseMetadata']['HTTPStatusCode'] == 204:
+            print("The {file} has deleted from {bucket} ...".format(file=file_path, bucket=bucket_name))
+        else:
+            raise Exception("Delete the {file} from {bucket} failed ...".format(file=file_path, bucket=bucket_name))
+
+        return response
 
 
-s3 = S3obj(end_point, s3_ac_key, s3_se_key)
+if __name__ == '__main__':
+    s3 = S3obj(end_point, s3_ac_key, s3_se_key)
 
-buckets = s3.list_files_from_bucket("test1")
+    buckets = s3.list_files_from_bucket("test1")
 
-print(buckets)
+    c = s3.delete_file("test1", '22222/')
+    print(c)
+    print(buckets)
--- snapshot/delete_snapshot.py
+++ snapshot/delete_snapshot.py
@@ -12,12 +12,12 @@ def del_volume(ssh_obj):
 
 
 def del_volumesnapshot(ssh_obj):
-    cmd = "kubectl get volumesnapshot|grep test|awk '{print $1}'|xargs kubectl delete volumesnapshot"
+    cmd = "kubectl get volumesnapshot|grep test|awk '{print $1}'|xargs kubectl delete volumesnapshot ;kubectl get volumesnapshotcontents.snapshot.storage.k8s.io|awk '{print $1}'|xargs kubectl delete volumesnapshotcontents.snapshot.storage.k8s.io"
     res = ssh_obj.run_cmd(cmd)
     logger.debug('\n' + res)
 
 
-@retry.retry(100, 3)
+@retry.retry(20, 3)
 def check_snapshotcotent_if_exist(ssh_obj):
     cmd = "kubectl get volumesnapshotcontents.snapshot.storage.k8s.io"
 
@@ -32,19 +32,24 @@ def check_snapshotcotent_if_exist(ssh_obj):
 
 
 def del_snapshot_yaml(ssh_obj, folder):
-    cmd = 'ls {folder}|grep shot|grep -v shot-1.yaml|xargs rm -rf'.format(folder=folder)
+    cmd = "cd {folder}; ls |grep shot|grep -v shot-1.yaml|xargs rm -rf".format(folder=folder)
     ssh_obj.run_cmd(cmd)
 
 
 if __name__ == '__main__':
 
     ssh = ssh_obj.SSHObj('10.180.116.11')
+    #1
     del_volume(ssh)
     del_volumesnapshot(ssh)
-    check_snapshotcotent_if_exist(ssh)
+    # check_snapshotcotent_if_exist(ssh)
 
+    #2
     folder1 = '/home/kwang/random/c-snapshot/'
     folder2 = '/home/kwang/random/r-snapshot/'
     del_snapshot_yaml(ssh, folder1)
     del_snapshot_yaml(ssh, folder2)
 
+    #3
+    cmd = "kubectl get po |grep -E 'csi|volume'|awk '{print $1}'|xargs kubectl delete po"
+    ssh.run_cmd(cmd)
