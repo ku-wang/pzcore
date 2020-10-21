32d450d44ba0f76850fef38ebf817febc0d63eec
Chris <kwang@panzura.com>
2020-10-21 11:15:07 +0800

    mongo

--- mongo/mongo-1.py
+++ mongo/mongo-1.py
@@ -5,7 +5,8 @@ import datetime
 import copy
 
 # template of data -> for test
-data_template = {"_id": 0, "loc": '', "name": '', "date": '', "company": "EVI"}
+data_template = {"loc": '', "name": '', "date": '', "company": "EVI", "department": "QA", "Group": "CORE",
+                 "Tester": "HandSomeChris"}
 locs = ["Shot_Guard", "Point_Guard", "Center", "Small_Forward", "Power_Forward"]
 ip = '10.180.116.17'
 port = 30515
@@ -28,20 +29,19 @@ class Mongo():
     def list_all_databases(self):
         return self._client.list_database_names()
 
-    def get_db_connection(self, db_name):
+    def get_db_obj(self, db_name):
         return self._client[db_name]
 
     def get_collection_obj(self, db_obj, connection_name):
         return db_obj[connection_name]
 
-    def list_all_collections(self, db_connection):
-        return db_connection.list_collection_names()
+    def list_all_collections(self, db_obj):
+        return db_obj.list_collection_names()
 
     def insert_data_by_collection(self, collection_obj, nums):
         templates = []
         for num in range(1, nums+1):
             template = copy.deepcopy(data_template)
-            template['_id'] = num
             template['loc'] = random.choice(locs)
             template['name'] = mongo_data.generate_random_string(6)
             template['date'] = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
@@ -55,49 +55,39 @@ class Mongo():
             raise Exception("Insert failed ...")
         return True
 
+
 def run(ip, port, db_name, collection_name, nums=10):
     mongo_obj = Mongo(ip, port)
 
-    db_obj = mongo_obj.get_db_connection(db_name)
+    db_obj = mongo_obj.get_db_obj(db_name)
     collection_obj = mongo_obj.get_collection_obj(db_obj, collection_name)
     mongo_obj.insert_data_by_collection(collection_obj, nums)
     for i in collection_obj.find():
         print(i)
 
 
-def run_multi_collctions(ip, port, db_name, collection_name, nums=10, collctions=10):
+def run_multi_collctions(ip, port, db_name, collection_name, file_nums=10, collctions=10):
     mongo_obj = Mongo(ip, port)
 
-    db_obj = mongo_obj.get_db_connection(db_name)
+    db_obj = mongo_obj.get_db_obj(db_name)
 
-    for colltion in range(collctions):
-        collection_obj = mongo_obj.get_collection_obj(db_obj, collection_name+str(colltion))
-        mongo_obj.insert_data_by_collection(collection_obj, nums)
-    # for i in collection_obj.find():
-    #     print(i)
-# mycol =
-# dic = {'loc': "Shot_Guard", "id": 10000}
-#
-# mycol.insert_one(dic)
-#
-# for i in range(1000):
-#     dic_ = {'loc': "Shot_Guard", "id": i}
-#     mycol.insert_one(dic_)
+    for collection in range(collctions):
+        collection_obj = mongo_obj.get_collection_obj(db_obj, collection_name+str(collection))
+        mongo_obj.insert_data_by_collection(collection_obj, file_nums)
 
-# cl_names = db.list_collection_names()
-# print(cl_names)
-# print(mycol.count())
-# print(db.NBA.find())
-# dblist = client.list_database_names()
 
-# print(dblist)
+def check_collections():
+    cl = MongoClient("mongodb://10.180.116.17:30515")
+    db = cl['chris_wang']
+    c = db.list_collection_names()
+    print(len(c))
+    coll = random.choice(c)
+
+    print(len(list(db[coll].find())))
 
 
 if __name__ == '__main__':
     # run(ip, port, database_for_test, cl_for_test, 1000)
-    run_multi_collctions(ip, port, database_for_test, cl_for_test, 1000)
-    # cl = MongoClient("mongodb://10.180.116.17:30515")
-    # print(cl.list_database_names())
-    # db = cl['chris-wang']
-    # c = db.list_collection_names()
-    # print(c)
+    run_multi_collctions(ip, port, database_for_test, cl_for_test, file_nums=1000, collctions=10)
+
+    check_collections()
